aa50be72b01bf99842d936be8507d6cb7fdc393c
Shengli Cui <shengli.cui@banmayun.com>
2014-04-21 11:14:20 +0800

    Initial Commit

---
+++ test/BanMaYunSDKTest/BanMaYunSDKTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BanMaYunSDKTest
+//
+//  Created by MeePoTech on 14-4-21.
+//  Copyright (c) 2014年 MeePoTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
